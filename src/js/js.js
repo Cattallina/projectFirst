f255ef531145988ae201f07bcb44735cb5bfa42a
Trina <cattallina017@gmail.com>
2021-02-24 18:41:51 +0200

    test

---
+++ src/js/js.js
@@ -0,0 +1,27 @@
+"use string";
+
+// const str = "test";
+// const arr = [1, 2, 3];
+//
+// console.log(str[2]);
+
+// const logg = "Hello, world!";
+// console.log(logg.slice(6, 12));
+
+// const test = "12.3px";
+// console.log(parseInt(test))
+
+// const arr = [1, 77, 2, 641, 48];
+//
+// arr.sort(comareNum);
+// console.log(arr);
+//
+// function comareNum(a, b) {
+//     return a - b;
+// }
+
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
